14711635a417c3e2c76edaa482c12bc1852f3021
mechen10 <melissa.c1010@gmail.com>
2017-06-09 13:15:08 -0700

    otu heatmap

--- OTUHeatmap.R
+++ OTUHeatmap.R
@@ -23,15 +23,15 @@ otuTableFP = opt$otuTableFP
 metadataFP = opt$metadata
 
 ########################### FOR TESTING #################################
-# setwd('/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/1_analysis')
-# inputFolder <- '/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/1_analysis/ALLMORPH_cores'
-# otuTableFP <- '/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/1_analysis/OTU_Table_text.txt'
-# metadataFP <- '/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/TEMP_frombotclust/MF_nochlpmito_m1000.txt'
+setwd('/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/1_analysis')
+inputFolder <- '/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/1_analysis/ALLMORPH_cores'
+otuTableFP <- '/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/1_analysis/OTU_Table_text.txt'
+metadataFP <- '/Users/melissachen/Documents/Masters/Project_Masters/Project_ArtificialMacroalgae/TEMP_frombotclust/MF_nochlpmito_m1000.txt'
 
-setwd('/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis')
-inputFolder <- '/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis/CORE/ALLMORPH_cores'
-otuTableFP <- '/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis/OTU_Table_text.txt'
-metadataFP <- '/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis/OTU_MP_filt/MF_nochlpmito_m1000.txt'
+# setwd('/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis')
+# inputFolder <- '/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis/CORE/ALLMORPH_cores'
+# otuTableFP <- '/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis/OTU_Table_text.txt'
+# metadataFP <- '/Users/parfreylab/Desktop/personal_files/melissa/ForBotanyCluster/z_AM/1_analysis/OTU_MP_filt/MF_nochlpmito_m1000.txt'
 
 
 ########################### LOAD DATA #################################
@@ -62,7 +62,7 @@ rownames(MF) <- gsub("-",".", rownames(MF))
 
 ListOfFiles <- system(paste0('ls ',inputFolder), intern = TRUE)
 
-P.Files <- ListOfFiles[grep("P_(BL|CR|FB|H201440|H2O20)", ListOfFiles)]
+P.Files <- ListOfFiles[grep("P_(BL|CR|FB|H2O1440|H2O20)", ListOfFiles)]
 H.Files <- ListOfFiles[grep("H_(BL|CR|FB|W)", ListOfFiles)]
 
 for (i in H.Files) {
@@ -119,150 +119,33 @@ H.Files.morphtime <- H.Files.time[c(grep('FB', H.Files.time)
 
 OTUTable.filt.col <- OTUTable.filt.col[,H.Files.morphtime]
 
-# Reorder OTUs
 
-
-# Make water core first
-water.Files <- H.Files[grep("W", H.Files)]
-coreOTUs.W <- c()
-for (i in water.Files) {
-  coreOTUs.W <- c(coreOTUs.W, rownames(get(i)))
-}
-coreOTUs.W <- unique(coreOTUs.W)
-
-# Core across all shapes?
-AM.Files <- H.Files[grep("CR|BL|FB", H.Files)][-grep("5760", H.Files)]
-maxcount <- length(AM.Files)
-coreOTUs.morphs <- c()
-for (i in AM.Files) {
-  coreOTUs.morphs <- c(coreOTUs.morphs, rownames(get(i)))
-}
-coreOTUs.morphs.all <- c()
-for (i in 1:length(table(coreOTUs.morphs)/maxcount)) {
-  if ((table(coreOTUs.morphs)/maxcount)[i] > 0.9) {
-    coreOTUs.morphs.all <- c(coreOTUs.morphs.all, rownames(table(coreOTUs.morphs))[i])
+########## Make core/not core matrix############
+OTUTable.core <- OTUTable.filt.col
+for (i in 1:nrow(OTUTable.core)) {
+  for (j in 1:ncol(OTUTable.core)) {
+    FileName <- H.Files[grep(colnames(OTUTable.core)[j], H.Files)]
+    if (rownames(OTUTable.core)[i] %in% rownames(get(FileName))) {
+      OTUTable.core[i,j] <- 1
+    } else {
+      OTUTable.core[i,j] <- 0
+      
+    }
   }
 }
 
-# Core across each shape
-# FB
-FB.Files <- H.Files[grep("FB", H.Files)]
-maxcount <- length(FB.Files)
-coreOTUs.FB <- c()
-for (i in FB.Files) {
-  coreOTUs.FB <- c(coreOTUs.FB, rownames(get(i)))
-}
-coreOTUs.FB.all <- c()
-coreOTUs.FB.leftover <- c()
-for (i in 1:length(table(coreOTUs.FB)/maxcount)) {
-  if ((table(coreOTUs.FB)/maxcount)[i] > 0.9) {
-    coreOTUs.FB.all <- c(coreOTUs.FB.all, rownames(table(coreOTUs.FB))[i])
+########## Presence/Absence matrix ############
+OTUTable.presabs <- OTUTable.filt.col
+for (i in 1:nrow(OTUTable.presabs)) {
+  for (j in 1:ncol(OTUTable.presabs)) {
+    if (OTUTable.presabs[i,j] > 0) {
+      OTUTable.presabs[i,j] <- 1
+    } else {
+      OTUTable.presabs[i,j] <- 0
+      
+    }
   }
 }
-# FB - 5760 without
-FB.Files.nolast <- FB.Files[-grep("5760", FB.Files)]
-maxcount <- length(FB.Files.nolast)
-coreOTUs.FB.nolast <- c()
-for (i in FB.Files.nolast) {
-  coreOTUs.FB.nolast <- c(coreOTUs.FB.nolast, rownames(get(i)))
-}
-coreOTUs.FB.all.nolast <- c()
-coreOTUs.FB.nolast.leftover <- c()
-for (i in 1:length(table(coreOTUs.FB.nolast)/maxcount)) {
-  if ((table(coreOTUs.FB.nolast)/maxcount)[i] > 0.9) {
-    coreOTUs.FB.all.nolast <- c(coreOTUs.FB.all.nolast, rownames(table(coreOTUs.FB.nolast))[i])
-  } 
-}
-FB.unique.final <- unique(c(coreOTUs.FB.all, coreOTUs.FB.all.nolast, coreOTUs.FB))
-
-# BL
-BL.Files <- H.Files[grep("BL", H.Files)]
-maxcount <- length(BL.Files)
-coreOTUs.BL <- c()
-for (i in BL.Files) {
-  coreOTUs.BL <- c(coreOTUs.BL, rownames(get(i)))
-}
-coreOTUs.BL.all <- c()
-coreOTUs.BL.leftover <- c()
-for (i in 1:length(table(coreOTUs.BL)/maxcount)) {
-  if ((table(coreOTUs.BL)/maxcount)[i] > 0.9) {
-    coreOTUs.BL.all <- c(coreOTUs.BL.all, rownames(table(coreOTUs.BL))[i])
-  }
-}
-# BL - 5760 without
-BL.Files.nolast <- BL.Files[-grep("5760", BL.Files)]
-maxcount <- length(BL.Files.nolast)
-coreOTUs.BL.nolast <- c()
-for (i in BL.Files.nolast) {
-  coreOTUs.BL.nolast <- c(coreOTUs.BL.nolast, rownames(get(i)))
-}
-coreOTUs.BL.all.nolast <- c()
-coreOTUs.BL.nolast.leftover <- c()
-for (i in 1:length(table(coreOTUs.BL.nolast)/maxcount)) {
-  if ((table(coreOTUs.BL.nolast)/maxcount)[i] > 0.9) {
-    coreOTUs.BL.all.nolast <- c(coreOTUs.BL.all.nolast, rownames(table(coreOTUs.BL.nolast))[i])
-  } 
-}
-BL.unique.final <- unique(c(coreOTUs.BL.all, coreOTUs.BL.all.nolast, coreOTUs.BL))
-
-
-
-# CR
-CR.Files <- H.Files[grep("CR", H.Files)]
-maxcount <- length(CR.Files)
-coreOTUs.CR <- c()
-for (i in CR.Files) {
-  coreOTUs.CR <- c(coreOTUs.CR, rownames(get(i)))
-}
-coreOTUs.CR.all <- c()
-coreOTUs.CR.leftover <- c()
-for (i in 1:length(table(coreOTUs.CR)/maxcount)) {
-  if ((table(coreOTUs.CR)/maxcount)[i] > 0.9) {
-    coreOTUs.CR.all <- c(coreOTUs.CR.all, rownames(table(coreOTUs.CR))[i])
-  }
-}
-# CR - 5760 without
-CR.Files.nolast <- CR.Files[-grep("5760", CR.Files)]
-maxcount <- length(CR.Files.nolast)
-coreOTUs.CR.nolast <- c()
-for (i in CR.Files.nolast) {
-  coreOTUs.CR.nolast <- c(coreOTUs.CR.nolast, rownames(get(i)))
-}
-coreOTUs.CR.all.nolast <- c()
-coreOTUs.CR.nolast.leftover <- c()
-for (i in 1:length(table(coreOTUs.CR.nolast)/maxcount)) {
-    if ((table(coreOTUs.CR.nolast)/maxcount)[i] > 0.9) {
-      coreOTUs.CR.all.nolast <- c(coreOTUs.CR.all.nolast, rownames(table(coreOTUs.CR.nolast))[i])
-    } 
-}
-CR.unique.final <- unique(c(coreOTUs.CR.all, coreOTUs.CR.all.nolast, coreOTUs.CR))
-
-# Only 5760
-H.Files.lastonly <- H.Files[grep("(BL|FB|CR)5760", H.Files)]
-maxcount <- length(H.Files.lastonly)
-coreOTUs.lastonly <- c()
-for (i in H.Files.lastonly) {
-  coreOTUs.lastonly <- c(coreOTUs.lastonly, rownames(get(i)))
-}
-coreOTUs.lastonly.all <- c()
-for (i in 1:length(table(coreOTUs.lastonly)/maxcount)) {
-  if ((table(coreOTUs.lastonly)/maxcount)[i] > 0.9) {
-    coreOTUs.lastonly.all <- c(coreOTUs.lastonly.all, rownames(table(coreOTUs.lastonly))[i])
-  }
-}
-
-
-# Now, sort OTUTable.filt.col OTUs
-ORDERED.otus <- unique(c(
-      coreOTUs.lastonly
-     , coreOTUs.morphs.all
-     , CR.unique.final
-     , BL.unique.final
-     , FB.unique.final
-     , coreOTUs.W 
-))
-
-OTUTable.filt.col <- OTUTable.filt.col[ORDERED.otus,]
 
 ################################# PLOT ####
 colortemp <- colorRampPalette(c("white","darkgreen"))
@@ -284,10 +167,12 @@ xlabels <- c("20 m"
   , "6 h"
   , "4 d")
 
+
 hc <- hclust(as.dist(1-cor(t(OTUTable.filt.col))))
 hc.v <- hclust(as.dist(1-cor(OTUTable.filt.col)))
 
-jpeg("./OTUHEATMAP/OTUHeatmap_Hakai_new.jpeg")
+
+pdf("./OTUHEATMAP/OTUHeatmap_Hakai_scaled.pdf", pointsize = 14)
 par(fig = c(0,1,0,1))
 heatmap.2(as.matrix(OTUTable.filt.col)
           , Colv = NA
@@ -313,18 +198,54 @@ legend("topright"
        )
 dev.off()
 
+pdf("./OTUHEATMAP/OTUHeatmap_Hakai_coreorot.pdf", pointsize = 14)
+heatmap.2(OTUTable.core
+          , Colv = NA
+          , Rowv = as.dendrogram(hc)
+          , dendrogram = 'none'
+          , cexCol = 1
+          , labRow = NA
+          , labCol = xlabels
+          , col = colortemp(2)
+          , margins = c(10,0.5)
+          , ColSideColors = c(rep("salmon",5),rep("dodgerblue",5),rep("darkorchid",5), rep("darkblue",2))
+          , scale = "none"
+          , density.info= "none"
+          , trace = "none"
+)
+par(fig = c(0,1,0,1))
+legend("topright"
+       , legend = c("Finely branched","Bladed","Crustose","Water")
+       , col = c("salmon","dodgerblue","darkorchid","darkblue")
+       , pch = 19
+       , cex = 0.7
+)
+dev.off()
 
-pdf(paste0("./OTUHEATMAP/OTUHeatmap_","Hakai",".pdf"), pointsize = 14)
-quartz()
-heatmap(as.matrix(OTUTable.filt.col)
-        , Colv = NA
-        , Rowv = NA
-        , cexCol = 0.7
-        , labRow = NA
-        , labCol = xlabels
-        , col = colortemp(10)
-        , margins = c(10,0.5)
-        )
+pdf("./OTUHEATMAP/OTUHeatmap_Hakai_presabs.pdf", pointsize = 14)
+par(fig = c(0,1,0,1))
+heatmap.2(OTUTable.presabs
+          , Colv = NA
+          , Rowv = as.dendrogram(hc)
+          , cexCol = 1
+          , labRow = NA
+          , labCol = xlabels
+          , col = colortemp(2)
+          , margins = c(10,0.5)
+          , density.info= "none"
+          , trace = "none"
+          # , breaks = c(0,0.00001,0.0001,0.001,0.01,0.1)
+          , dendrogram = 'none'
+          , ColSideColors = c(rep("salmon",5),rep("dodgerblue",5),rep("darkorchid",5), rep("darkblue",2))
+          , scale = "none"
+)
+par(fig = c(0,1,0,1))
+legend("topright"
+       , legend = c("Finely branched","Bladed","Crustose","Water")
+       , col = c("salmon","dodgerblue","darkorchid","darkblue")
+       , pch = 19
+       , cex = 0.7
+)
 dev.off()
 
 
@@ -369,182 +290,134 @@ P.Files.morphtime <- P.Files.time[c(grep('FB', P.Files.time)
 
 OTUTable.filt.col <- OTUTable.filt.col[,P.Files.morphtime]
 
-# Reorder OTUs
-
-
-# Make water core first
-water.Files <- P.Files[grep("H2O", P.Files)]
-coreOTUs.W <- c()
-for (i in water.Files) {
-  coreOTUs.W <- c(coreOTUs.W, rownames(get(i)))
-}
-coreOTUs.W <- unique(coreOTUs.W)
-
-# Core across all shapes?
-AM.Files <- P.Files[grep("CR|BL|FB", P.Files)]
-maxcount <- length(AM.Files)
-coreOTUs.morphs <- c()
-for (i in AM.Files) {
-  coreOTUs.morphs <- c(coreOTUs.morphs, rownames(get(i)))
-}
-coreOTUs.morphs.all <- c()
-for (i in 1:length(table(coreOTUs.morphs)/maxcount)) {
-  if ((table(coreOTUs.morphs)/maxcount)[i] > 0.9) {
-    coreOTUs.morphs.all <- c(coreOTUs.morphs.all, rownames(table(coreOTUs.morphs))[i])
+########## Make core/not core matrix############
+OTUTable.core <- OTUTable.filt.col
+for (i in 1:nrow(OTUTable.core)) {
+  for (j in 1:ncol(OTUTable.core)) {
+    FileName <- P.Files[grep(colnames(OTUTable.core)[j], P.Files)]
+    if (rownames(OTUTable.core)[i] %in% rownames(get(FileName))) {
+      OTUTable.core[i,j] <- 1
+    } else {
+      OTUTable.core[i,j] <- 0
+      
+    }
   }
 }
 
-# Core across each shape
-# FB
-FB.Files <- P.Files[grep("FB", P.Files)]
-maxcount <- length(FB.Files)
-coreOTUs.FB <- c()
-for (i in FB.Files) {
-  coreOTUs.FB <- c(coreOTUs.FB, rownames(get(i)))
-}
-coreOTUs.FB.all <- c()
-coreOTUs.FB.leftover <- c()
-for (i in 1:length(table(coreOTUs.FB)/maxcount)) {
-  if ((table(coreOTUs.FB)/maxcount)[i] > 0.9) {
-    coreOTUs.FB.all <- c(coreOTUs.FB.all, rownames(table(coreOTUs.FB))[i])
+########## Presence/Absence matrix ############
+OTUTable.presabs <- OTUTable.filt.col
+for (i in 1:nrow(OTUTable.presabs)) {
+  for (j in 1:ncol(OTUTable.presabs)) {
+    if (OTUTable.presabs[i,j] > 0) {
+      OTUTable.presabs[i,j] <- 1
+    } else {
+      OTUTable.presabs[i,j] <- 0
+      
+    }
   }
 }
-# # FB - 5760 without
-# FB.Files.nolast <- FB.Files[-grep("5760", FB.Files)]
-# maxcount <- length(FB.Files.nolast)
-# coreOTUs.FB.nolast <- c()
-# for (i in FB.Files.nolast) {
-#   coreOTUs.FB.nolast <- c(coreOTUs.FB.nolast, rownames(get(i)))
-# }
-# coreOTUs.FB.all.nolast <- c()
-# coreOTUs.FB.nolast.leftover <- c()
-# for (i in 1:length(table(coreOTUs.FB.nolast)/maxcount)) {
-#   if ((table(coreOTUs.FB.nolast)/maxcount)[i] > 0.9) {
-#     coreOTUs.FB.all.nolast <- c(coreOTUs.FB.all.nolast, rownames(table(coreOTUs.FB.nolast))[i])
-#   } 
-# }
-FB.unique.final <- unique(c(coreOTUs.FB.all, coreOTUs.FB))
 
-# BL
-BL.Files <- P.Files[grep("BL", P.Files)]
-maxcount <- length(BL.Files)
-coreOTUs.BL <- c()
-for (i in BL.Files) {
-  coreOTUs.BL <- c(coreOTUs.BL, rownames(get(i)))
-}
-coreOTUs.BL.all <- c()
-coreOTUs.BL.leftover <- c()
-for (i in 1:length(table(coreOTUs.BL)/maxcount)) {
-  if ((table(coreOTUs.BL)/maxcount)[i] > 0.9) {
-    coreOTUs.BL.all <- c(coreOTUs.BL.all, rownames(table(coreOTUs.BL))[i])
-  }
-}
-# # BL - 5760 without
-# BL.Files.nolast <- BL.Files[-grep("5760", BL.Files)]
-# maxcount <- length(BL.Files.nolast)
-# coreOTUs.BL.nolast <- c()
-# for (i in BL.Files.nolast) {
-#   coreOTUs.BL.nolast <- c(coreOTUs.BL.nolast, rownames(get(i)))
-# }
-# coreOTUs.BL.all.nolast <- c()
-# coreOTUs.BL.nolast.leftover <- c()
-# for (i in 1:length(table(coreOTUs.BL.nolast)/maxcount)) {
-#   if ((table(coreOTUs.BL.nolast)/maxcount)[i] > 0.9) {
-#     coreOTUs.BL.all.nolast <- c(coreOTUs.BL.all.nolast, rownames(table(coreOTUs.BL.nolast))[i])
-#   } 
-# }
-BL.unique.final <- unique(c(coreOTUs.BL.all, coreOTUs.BL))
-
-
-
-# CR
-CR.Files <- P.Files[grep("CR", P.Files)]
-maxcount <- length(CR.Files)
-coreOTUs.CR <- c()
-for (i in CR.Files) {
-  coreOTUs.CR <- c(coreOTUs.CR, rownames(get(i)))
-}
-coreOTUs.CR.all <- c()
-coreOTUs.CR.leftover <- c()
-for (i in 1:length(table(coreOTUs.CR)/maxcount)) {
-  if ((table(coreOTUs.CR)/maxcount)[i] > 0.9) {
-    coreOTUs.CR.all <- c(coreOTUs.CR.all, rownames(table(coreOTUs.CR))[i])
-  }
-}
-# # CR - 5760 without
-# CR.Files.nolast <- CR.Files[-grep("5760", CR.Files)]
-# maxcount <- length(CR.Files.nolast)
-# coreOTUs.CR.nolast <- c()
-# for (i in CR.Files.nolast) {
-#   coreOTUs.CR.nolast <- c(coreOTUs.CR.nolast, rownames(get(i)))
-# }
-# coreOTUs.CR.all.nolast <- c()
-# coreOTUs.CR.nolast.leftover <- c()
-# for (i in 1:length(table(coreOTUs.CR.nolast)/maxcount)) {
-#   if ((table(coreOTUs.CR.nolast)/maxcount)[i] > 0.9) {
-#     coreOTUs.CR.all.nolast <- c(coreOTUs.CR.all.nolast, rownames(table(coreOTUs.CR.nolast))[i])
-#   } 
-# }
-CR.unique.final <- unique(c(coreOTUs.CR.all, coreOTUs.CR))
-
-# # Only 5760
-# P.Files.lastonly <- P.Files[grep("(BL|FB|CR)5760", P.Files)]
-# maxcount <- length(P.Files.lastonly)
-# coreOTUs.lastonly <- c()
-# for (i in P.Files.lastonly) {
-#   coreOTUs.lastonly <- c(coreOTUs.lastonly, rownames(get(i)))
-# }
-# coreOTUs.lastonly.all <- c()
-# for (i in 1:length(table(coreOTUs.lastonly)/maxcount)) {
-#   if ((table(coreOTUs.lastonly)/maxcount)[i] > 0.9) {
-#     coreOTUs.lastonly.all <- c(coreOTUs.lastonly.all, rownames(table(coreOTUs.lastonly))[i])
-#   }
-# }
-
-
-# Now, sort OTUTable.filt.col OTUs
-ORDERED.otus <- unique(c(
-  # coreOTUs.lastonly
-   coreOTUs.morphs.all
-  , CR.unique.final
-  , BL.unique.final
-  , FB.unique.final
-  , coreOTUs.W 
-))
-
-OTUTable.filt.col <- OTUTable.filt.col[ORDERED.otus,]
 
 ################################# PLOT ####
 colortemp <- colorRampPalette(c("white","darkgreen"))
-xlabels <- c("Finely Branched: 20 m"
-             , "Finely Branched: 1 h"
-             , "Finely Branched: 3 h"
-             , "Finely Branched: 6 h"
-             , "Finely Branched: 12 h"
-             , "Finely Branched: 1 d"
-             , "Bladed: 20 m"
-             , "Bladed: 1 h"
-             , "Bladed: 3 h"
-             , "Bladed: 6 h"
-             , "Bladed: 12 h"
-             , "Bladed: 1 d"
-             , "Crustose: 20 m"
-             , "Crustose: 1 h"
-             , "Crustose: 3 h"
-             , "Crustose: 6 h"
-             , "Crustose: 12 h"
-             , "Crustose: 1 d"
-             , "Water: 20 m"
-             , "Water: 1 d")
+xlabels <- c("20 m"
+             , "1 h"
+             , "3 h"
+             , "6 h"
+             , "12 h"
+             , "1 d"
+             , "20 m"
+             , "1 h"
+             , "3 h"
+             , "6 h"
+             , "12 h"
+             , "1 d"
+             , "20 m"
+             , "1 h"
+             , "3 h"
+             , "6 h"
+             , "12 h"
+             , "1 d"
+             , "20 m"
+             , "1 d")
 
-pdf(paste0("./OTUHEATMAP/OTUHeatmap_","PM",".pdf"), pointsize = 14)
-heatmap(as.matrix(OTUTable.filt.col)
-        , Colv = NA
-        , Rowv = NA
-        , cexCol = 0.7
-        , labRow = NA
-        , labCol = xlabels
-        , col = colortemp(10)
-        , margins = c(10,0.5)
+hc <- hclust(as.dist(1-cor(t(OTUTable.filt.col))))
+hc.v <- hclust(as.dist(1-cor(OTUTable.filt.col)))
+
+pdf("./OTUHEATMAP/OTUHeatmap_PM_scaled.pdf", pointsize = 14)
+par(fig = c(0,1,0,1))
+heatmap.2(as.matrix(OTUTable.filt.col)
+          , Colv = NA
+          , Rowv = as.dendrogram(hc)
+          , cexCol = 1
+          , labRow = NA
+          , labCol = xlabels
+          , col = colortemp(10)
+          , margins = c(10,0.5)
+          , density.info= "none"
+          , trace = "none"
+          # , breaks = c(0,0.00001,0.0001,0.001,0.01,0.1)
+          , dendrogram = 'none'
+          , ColSideColors = c(rep("salmon",6),rep("dodgerblue",6),rep("darkorchid",6), rep("darkblue",2))
+          , scale = "row"
+)
+par(fig = c(0,1,0,1))
+legend("topright"
+       , legend = c("Finely branched","Bladed","Crustose","Water")
+       , col = c("salmon","dodgerblue","darkorchid","darkblue")
+       , pch = 19
+       , cex = 0.7
+)
+dev.off()
+
+pdf("./OTUHEATMAP/OTUHeatmap_PM_coreornot.pdf", pointsize = 14)
+par(fig = c(0,1,0,1))
+heatmap.2(as.matrix(OTUTable.core)
+          , Colv = NA
+          , Rowv = as.dendrogram(hc)
+          , cexCol = 1
+          , labRow = NA
+          , labCol = xlabels
+          , col = colortemp(2)
+          , margins = c(10,0.5)
+          , density.info= "none"
+          , trace = "none"
+          # , breaks = c(0,0.00001,0.0001,0.001,0.01,0.1)
+          , dendrogram = 'none'
+          , ColSideColors = c(rep("salmon",6),rep("dodgerblue",6),rep("darkorchid",6), rep("darkblue",2))
+          , scale = "none"
+)
+par(fig = c(0,1,0,1))
+legend("topright"
+       , legend = c("Finely branched","Bladed","Crustose","Water")
+       , col = c("salmon","dodgerblue","darkorchid","darkblue")
+       , pch = 19
+       , cex = 0.7
+)
+dev.off()
+
+pdf("./OTUHEATMAP/OTUHeatmap_PM_presabs.pdf", pointsize = 14)
+par(fig = c(0,1,0,1))
+heatmap.2(as.matrix(OTUTable.presabs)
+          , Colv = NA
+          , Rowv = as.dendrogram(hc)
+          , cexCol = 1
+          , labRow = NA
+          , labCol = xlabels
+          , col = colortemp(2)
+          , margins = c(10,0.5)
+          , density.info= "none"
+          , trace = "none"
+          # , breaks = c(0,0.00001,0.0001,0.001,0.01,0.1)
+          , dendrogram = 'none'
+          , ColSideColors = c(rep("salmon",6),rep("dodgerblue",6),rep("darkorchid",6), rep("darkblue",2))
+          , scale = "none"
+)
+par(fig = c(0,1,0,1))
+legend("topright"
+       , legend = c("Finely branched","Bladed","Crustose","Water")
+       , col = c("salmon","dodgerblue","darkorchid","darkblue")
+       , pch = 19
+       , cex = 0.7
 )
 dev.off()
